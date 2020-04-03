737f0c1132cbb44e92c710d7b27a2bedd45187ba
Nevets08 <Nevets08@users.noreply.github.com>
2020-04-03 21:55:16 +0200

    Install + construct

---
+++ var/cache/prod/smarty/compile/layouts_layout_left_column_tpl/2b/98/79/2b98797fcc45aa30e47f1eb605976469d76e94a6_2.file.category-header.tpl.php
@@ -0,0 +1,49 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-04-03 20:05:53
+  from 'C:\Users\steven\Documents\Prestashop_projet\themes\classic\templates\catalog\_partials\category-header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5e877b0141aad0_70654131',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2b98797fcc45aa30e47f1eb605976469d76e94a6' => 
+    array (
+      0 => 'C:\\Users\\steven\\Documents\\Prestashop_projet\\themes\\classic\\templates\\catalog\\_partials\\category-header.tpl',
+      1 => 1579532616,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e877b0141aad0_70654131 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="js-product-list-header">
+    <?php if ($_smarty_tpl->tpl_vars['listing']->value['pagination']['items_shown_from'] == 1) {?>
+        <div class="block-category card card-block">
+            <h1 class="h1"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['category']->value['name'], ENT_QUOTES, 'UTF-8');?>
+</h1>
+            <div class="block-category-inner">
+                <?php if ($_smarty_tpl->tpl_vars['category']->value['description']) {?>
+                    <div id="category-description" class="text-muted"><?php echo $_smarty_tpl->tpl_vars['category']->value['description'];?>
+</div>
+                <?php }?>
+                <?php if ($_smarty_tpl->tpl_vars['category']->value['image']['large']['url']) {?>
+                    <div class="category-cover">
+                        <img src="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['category']->value['image']['large']['url'], ENT_QUOTES, 'UTF-8');?>
+" alt="<?php if (!empty($_smarty_tpl->tpl_vars['category']->value['image']['legend'])) {
+echo htmlspecialchars($_smarty_tpl->tpl_vars['category']->value['image']['legend'], ENT_QUOTES, 'UTF-8');
+} else {
+echo htmlspecialchars($_smarty_tpl->tpl_vars['category']->value['name'], ENT_QUOTES, 'UTF-8');
+}?>">
+                    </div>
+                <?php }?>
+            </div>
+        </div>
+    <?php }?>
+</div>
+<?php }
+}
